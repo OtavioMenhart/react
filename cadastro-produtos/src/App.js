a3ecf3a1f48fc225fd1e151aa8516c8742e67b6e
Otávio Menhart <otaviomenhart@hotmail.com>
2021-08-24 11:24:55 -0300

    implementando navbar

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,12 +1,10 @@
-
+import Navbar from './components/navbar'
 
 function App() {
   return (
-    <div className="alert alert-dismissible alert-warning">
-      <button type="button" className="btn-close" data-bs-dismiss="alert"></button>
-      <h4 className="alert-heading">Warning!</h4>
-      <p className="mb-0">hello.</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
