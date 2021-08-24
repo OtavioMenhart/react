fcbeda4f97a1632f614f6a5ed27fb818727218b3
Otávio Menhart <otaviomenhart@hotmail.com>
2021-08-24 11:55:32 -0300

    início formulário de produtos

--- cadastro-produtos/src/views/produtos/cadastro.js
+++ cadastro-produtos/src/views/produtos/cadastro.js
@@ -1,15 +1,62 @@
 import React from 'react';
 
-export default class CadastroProduto extends React.Component{
+export default class CadastroProduto extends React.Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div className="card">
                 <div className="card-header">
                     Cadastro de Produto
                 </div>
                 <div className="card-body">
-                    
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Nome: *</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>SKU: *</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label>Descrição: *</label>
+                                <textarea className="form-control" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Preço: *</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="form-group">
+                                <label>Fornecedor: *</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <br />
+                    <div className="row">
+                        <div className="col-md-1">
+                            <button className="btn btn-success">Salvar</button>
+                        </div>
+                        <div className="col-md-1">
+                            <button className="btn btn-primary">Limpar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
