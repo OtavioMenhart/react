dd92518c1e2b5e4e703b19e335795aed484827b3
Otávio Menhart <otaviomenhart@hotmail.com>
2021-09-01 18:54:43 -0300

    introdução ao react funcional, utilizando useState

---
+++ react-funcional/src/App.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+
+function App() {
+
+  const [numero, setNumero] = useState();
+  const [segundoNumero, setSegundoNumero] = useState();
+  const [resultado, setResultado] = useState();
+
+  const somar = () => {
+    const numeroInt = parseInt(numero);
+    const segNumeroInt = parseInt(segundoNumero);
+
+    setResultado(numeroInt + segNumeroInt);
+  }
+
+  // const [state, setState] = useState({
+  //   numero: 0,
+  //   segundoNumero: 0,
+  //   resultado: 0
+  // });
+
+  return (
+    <div>
+      Número 1: <br/>
+      <input type="text" value={numero} onChange={e => setNumero(e.target.value)} />
+      <br />
+
+      Número 2: <br/>
+      <input type="text" value={segundoNumero} onChange={e => setSegundoNumero(e.target.value)}/>
+      <br />
+      <button onClick={somar}>Somar</button> <br/>
+      Resultado: <br/>
+      <input type="text" value={resultado} />
+    </div>
+  );
+}
+
+export default App;
